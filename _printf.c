5bdf764e2da299f195fa938a1b38c785eb56c0fc
Brightson634 <brightsontwine@gmail.com>
2022-08-15 15:16:07 -0700

    print f

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
-lude "main.h"
-
+#include "main.h"
 #include <stdarg.h>
 
 
@@ -9,14 +8,10 @@ void print_buffer(char buffer[], int *buff_ind);
 
 
 /**
-
  * _printf - Printf function.
-
- * @format: format.
-
- * Return: Printed chars..
-
- */
+* @format: format.
+* Return: Printed chars..
+*/
 
 int _printf(const char *format, ...)
 
@@ -109,14 +104,10 @@ int _printf(const char *format, ...)
 
 
 /**
-
- * print_buffer - Prints the contents of the buffer if it exist
-
- * @buffer: Array of chars
-
- * @buff_ind: Index at which to add next char, represents the length.
-
- */
+* print_buffer - Prints the contents of the buffer if it exist
+* @buffer: Array of chars
+* @buff_ind: Index at which to add next char, represents the length.
+*/
 
 void print_buffer(char buffer[], int *buff_ind)
 
